7c7725bcdc4091a9144c14f26470672361c6912d
Tim Lonsdale <maambmb@Tims-MBP.lan>
2017-06-06 14:35:19 +0100

    foo

---
+++ index.js
@@ -0,0 +1,37 @@
+class Logger {
+
+    constructor( fmtter ) {
+        this.fmtter = fmtter || null;
+    }
+
+    info( ctx ) {
+        console.log( JSON.stringify( this.log( "INFO", ctx ) ) );
+    }
+
+    warn( ctx ) {
+        console.log( JSON.stringify( this.log( "WARN", ctx ) ) );
+    }
+
+    error( ctx ) {
+        console.log( JSON.stringify( this.log( "ERROR", ctx ) ) );
+    }
+
+    fatal( ctx ) {
+        console.log( JSON.stringify( this.log( "FATAL", ctx ) ) );
+    }
+
+    log( level, ctx )  {
+
+        if( this.fmtter )
+            return this.fmtter( level, ctx );
+
+        return {
+            level : level,
+            ctx   : ctx,
+            ts    : new Date().getTime(),
+        };
+    }
+
+}
+
+module.exports = Logger;
