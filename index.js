ea142245a3839052a1acdb891c59b54578cd96ec
Tim Lonsdale <maambmb@Tims-MBP.lan>
2017-06-23 18:33:50 +0100

    tidy

--- index.js
+++ index.js
@@ -8,24 +8,12 @@ class Logger {
         console.log( this.log( "INFO", msg ) );
     }
 
-    alert( msg ) {
-        console.log( this.log( "ALERT", msg ) );
-    }
-
-    success( msg ) {
-        console.log( this.log( "SUCCESS", msg ) );
-    }
-
     warn( msg ) {
         console.log( this.log( "WARN", msg ) );
     }
 
     error( msg ) {
         console.log( this.log( "ERROR", msg ) );
-    }
-
-    fatal( msg ) {
-        console.log( this.log( "FATAL", msg ) );
     }
 
     log( level, msg )  {
